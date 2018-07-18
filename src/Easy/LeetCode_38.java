de8fa7b5556db7dd3bafb392b3464ce6b28b2711
XuZiyu <1269131995@qq.com>
2018-07-18 21:49:38 +0800

    新增38号算法题目

---
+++ src/Easy/LeetCode_38.java
@@ -0,0 +1,23 @@
+package Easy;
+
+/**
+ * 项目名称：LeetCode
+ * 包名：Easy
+ * 作者：X-Z-Y
+ * 时间：2018-7-17 21:58
+ * 描述：报数序列是指一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ */
+public class LeetCode_38 {
+    public LeetCode_38() {
+
+    }
+
+    private static String mine(int n){
+
+    }
+}
